f6a9fbbc897232652815b97dd737b4cc8a0f0d17
pugatin <56666131+pugatin@users.noreply.github.com>
2019-10-18 11:05:17 +0700

    kh,g

--- q.py
+++ q.py
@@ -1,6 +1,7 @@
 import telebot
 from telebot import types
-ans= 'Заказ'
+import random
+
 bot = telebot.TeleBot("941558477:AAGShts4Z6AFqc1E5LEc3zxi5YdpzxJ4OKk")
 #ans1 = 1
 #ans2=2
@@ -14,58 +15,98 @@ def hello(message):
 ans=1
 
 '''
-
+arr = ['']*6
+count = [0]*2
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True)
 keyboard1.row('сильвермолл', 'новый')
-keyboard2 = telebot.types.ReplyKeyboardMarkup(True)
+keyboard2 = telebot.types.ReplyKeyboardMarkup(True) 
 keyboard2.row('кфс', 'сабвей')
 keyboard3 = telebot.types.ReplyKeyboardMarkup(True)
 keyboard3.row('бургер', 'картошка')
+keyboardChoose = telebot.types.ReplyKeyboardMarkup(True)
+keyboardChoose.row('да', 'нет')
 keyboard4 = telebot.types.ReplyKeyboardMarkup(True)
 keyboard4.row('самовывоз', 'доставка')
-keyboard5 = telebot.types.ReplyKeyboardMarkup(True, True)                    #89646563493
+keyboard5 = telebot.types.ReplyKeyboardMarkup(True, True)                   
 keyboard5.row('оформить заказ')
+
+
 @bot.message_handler(commands = ['start'])
 def start_message(message):
 	bot.send_message(message.chat.id, "в какой тц пойдем?", reply_markup=keyboard1)
 
+
+
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-	ans='заказон'
-	if message.text.lower() == 'сильвермолл':
-		bot.send_message(message.chat.id, "в какой ресторан пойдем?", reply_markup=keyboard2)	
-		bot.send_message(745923507, '{name} заказал в ТЦ {order}'.format(order='сильвермолл', name=message.from_user.first_name))
-		
-	elif message.text.lower() == 'новый':
+	ans = message.text.lower()
+	
+	if ans == 'сильвермолл':
+		bot.send_message(message.chat.id, "в какой ресторан пойдем?", reply_markup=keyboard2)			
+		arr[0] = ans
+
+	elif ans == 'новый':
 		bot.send_message(message.chat.id, "в какой ресторан пойдем?", reply_markup=keyboard2)
-		bot.send_message(745923507, '{name} заказал в ТЦ {order}'.format(order='сильвермолл', name=message.from_user.first_name))
+		arr[0] = 'новый'
 		
-	elif message.text.lower() == 'кфс':
+	elif ans == 'кфс' or message.text.lower() == 'нет':
 		bot.send_message(message.chat.id, "что будем есть?", reply_markup=keyboard3)
-		bot.send_message(745923507, '{order}'.format(order='кфс'))
-	elif message.text.lower() == 'сабвей':
+		if arr[1] != 'кфс':			
+			arr[1] = ans
+
+	elif ans == 'сабвей' or message.text.lower() == 'нет':
 		bot.send_message(message.chat.id, "что будем есть?", reply_markup=keyboard3)
-		bot.send_message(745923507, '{order}'.format(order='сабвей'))
-	elif message.text.lower() == 'бургер':
-		bot.send_message(message.chat.id, "самовывоз или доставка?", reply_markup=keyboard4)
-		bot.send_message(745923507, '{order}'.format(order='бургер'))
-	elif message.text.lower() == 'картошка':
-		bot.send_message(message.chat.id, "самовывоз или доставка?", reply_markup=keyboard4)
-		bot.send_message(745923507, '{order}'.format(order='картошка'))
-	elif message.text.lower() == 'самовывоз':
+		if arr[1] != 'сабвей':
+			arr[1] = ans
+
+	elif ans == 'бургер':
+		bot.send_message(message.chat.id, "закончить покупки?", reply_markup=keyboardChoose)					
+		arr[2] = ans
+		count[0] += 1
+
+	elif ans == 'картошка':
+		bot.send_message(message.chat.id, "закончить покупки?", reply_markup=keyboardChoose)
+		arr[3] = 'картошка'
+		count[1] += 1
+
+	elif ans == 'да':
+		bot.send_message(message.chat.id, 'самовывоз или доставка?', reply_markup = keyboard4)
+
+	elif ans == 'самовывоз':	
 		bot.send_message(message.chat.id, "оформить заказ?", reply_markup=keyboard5)
-		bot.send_message(745923507, '{order}'.format(order='самовывоз'))
-	elif message.text.lower() == 'доставка':
+		arr[4] = ans
+
+	elif ans == 'доставка':
 		bot.send_message(message.chat.id, "оформить заказ?", reply_markup=keyboard5)
-		bot.send_message(745923507, '{order}'.format(order='доставка'))
-	elif message.text.lower() == 'оформить заказ':
-		bot.send_message(745923507, '{order}'.format(order='заказ готов'))
-		bot.send_message(745923507, '{rder}'.format(rder=a))
+		arr[4] = ans
+
+	elif ans == 'оформить заказ':
+		
+		
+		rez = ''	
 		bot.send_message(message.chat.id, 'Твой заказ принят')
+		for i in range(len(arr)):
+			if i == 2 and count[0] != 0:
+				rez += str(count[0]) + ' - ' +  arr[2] + ' -> '
+				continue
+			elif i == 3 and count[1] != 0:
+				rez += str(count[1]) + ' - ' + arr[3] + ' -> '
+				continue
+			elif i == 4:
+				rez += arr[i]
+				break
+			else:
+				rez += arr[i] + ' -> '
+		req = str(random.randint(1, 1000)) + ' ' + message.from_user.first_name + ' ' +  message.from_user.second_name
+		rez += '    время - ' + order_time
+		rez += '    реквизиты: ' + req
+		bot.send_message(785534105, '{order}'.format(order=rez))
+               
 
 	
 
 bot.polling(none_stop = True)
 
 
- #ест - меню - способ доставки - если самовывоз то время когда забрать заказ - оплата
+
+
